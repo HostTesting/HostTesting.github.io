7bddb38d94d00251f38013dd859e08f0872b038e
HostTesting <123798664+HostTesting@users.noreply.github.com>
2023-02-06 21:44:34 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,20 +5,23 @@ function updateProgress(percent) {
     progress.style.width = percent + "%";
   }
 
-document.addEventListener('keydown', function check(){
+
+var input = document.getElementById("input");
+input.addEventListener('input', function check(){
     var text = document.getElementById("text");
     var progress = document.getElementById("input").value;
     if (progress[x] === text.innerText[x]) {
         text.classList.add("right")
         text.classList.remove("wrong")
         console.log(progress)
-        x += 1
+        x = progress.length
         updateProgress(x/6.5)
         return x
     }
     else if (progress[x] !== text.innerText[x]){
         text.classList.add("wrong")
         text.classList.remove("right")
+        x = progress.length
     }
 })
 
@@ -35,7 +38,8 @@ function submit(){
 }
 
 var timer = document.getElementById("timer");
-var remainingSeconds = 60;
+timer.style.color = "gold"
+var remainingSeconds = 10;
 function start(){
     var y = setInterval(function() {
         if (remainingSeconds > 0) {
@@ -44,12 +48,18 @@ function start(){
         } else {
         clearInterval(y);
         timer.innerHTML = "RESTART";
-        if(text.classList.contains("right") === true){
+        if(text.classList.contains("right") === true && x < 600){
             alert("Your time is up")
             alert("Your typing speed is "+ Number(x/5) + " WPM")
         }
-        else if (text.classList.contains("wrong") && x < 10){
-            alert("You're either a copy pasta cheater or you're really slow")
+        else if (text.classList.contains("wrong") === true && x < 600){
+            alert("You made a mistake")
+        }
+        else if (x > 600){
+            alert("You copy pasta cheater!")
+            alert("!!!!!!!!!!!")
+            alert("You dare cheat!!!!!!!!!!")
+            text.classList.add("wrong")
         }
         }
         console.log(x)
